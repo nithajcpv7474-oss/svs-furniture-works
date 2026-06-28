3d98dc6390a4d74386013c963691c3997fd1ce91
SVS Deploy <deploy@svsfurniture.com>
2026-06-28 10:50:40 +0530

    Fix CORS issue blocking Vercel frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,7 +30,12 @@ app.set('trust proxy', 1);
 
 // Configure CORS
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+  origin: [
+    'http://localhost:5173', 
+    'http://localhost:5174', 
+    'http://localhost:5175', 
+    'https://svs-furniture-works.vercel.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
